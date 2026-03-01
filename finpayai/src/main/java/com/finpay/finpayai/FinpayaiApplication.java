37cc3aaa6833393d8f999dba78aaa48f5150095b
Anoop21-oss-a <anoopkumarpatel935@gmail.com>
2026-03-01 11:08:01 +0530

    create java springboot web app

---
+++ finpayai/src/main/java/com/finpay/finpayai/FinpayaiApplication.java
@@ -0,0 +1,13 @@
+package com.finpay.finpayai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinpayaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinpayaiApplication.class, args);
+	}
+
+}
---
+++ finpayai/src/test/java/com/finpay/finpayai/FinpayaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finpay.finpayai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinpayaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
